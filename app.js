2d135086058c30f88e4912021f6228245d47163d
Ka Wai Wong <kw.wong0923@gmail.com>
2023-09-12 19:21:37 -0400

    modified basic setting, built connection of db

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Initialize
+require("dotenv").config();
+require("express-async-errors");
+// ---------- Import ----------
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+// Cloud Storage
+const fileUpload = require("express-fileupload");
+// Cloudinary and it's configuration
+const { v2: cloudinary } = require("cloudinary");
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Passport
+const passport = require("passport");
+const session = require("express-session");
 
-var app = express();
+// Security Packages
+const cors = require("cors");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// MongoDB
+const connectDB = require("./db/connect");
 
-app.use(logger('dev'));
+// Router
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+// ---------- Middlewares ----------
+let app = express();
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Server Listening
+const port = process.env.PORT || 5000;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    console.log("Connect to Database");
+    app.listen(port, () => {
+      console.log(`The server is running on port: ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+start();
