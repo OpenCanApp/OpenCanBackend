7cdd021d9eae5ba9513d5f749709db32508d0d88
Ka Wai Wong <kw.wong0923@gmail.com>
2023-10-26 02:14:30 -0400

    Testing for google

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -3,11 +3,17 @@ const { User } = require("../models");
 const CustomError = require("../errors");
 const { createTokenUser, createJWT } = require("../utils");
 
+// const googleLogin = async (req, res) => {
+//   const tokenUser = createTokenUser(req.user);
+//   const token = createJWT(tokenUser);
+
+//   res.status(StatusCodes.OK).json({ user: tokenUser, token });
+// };
+
 const googleLogin = async (req, res) => {
   const tokenUser = createTokenUser(req.user);
   const token = createJWT(tokenUser);
-
-  res.status(StatusCodes.OK).json({ user: tokenUser, token });
+  res.redirect(`myapp://callback?token=${token}`);
 };
 
 const facebookLogin = async (req, res) => {
